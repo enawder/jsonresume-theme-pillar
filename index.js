2f3cca4675f8617dedf448cd667719f41e8e6a18
enawder <60314455+enawder@users.noreply.github.com>
2023-03-01 20:04:10 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+var path = require('path');
+var Handlebars = require("handlebars");
+
+function render(resume) {
+	var css = fs.readFileSync(__dirname + "/src/pillar-theme/assets/css/pillar-1.css", "utf-8");
+	var tpl = fs.readFileSync(__dirname + "/src/index.hbs", "utf-8");
+	var partialsDir = path.join(__dirname, '/src/partials');
+	var filenames = fs.readdirSync(partialsDir);
+
+	filenames.forEach(function (filename) {
+	  var matches = /^([^.]+).hbs$/.exec(filename);
+	  if (!matches) {
+	    return;
+	  }
+	  var name = matches[1];
+	  var filepath = path.join(partialsDir, filename)
+	  var template = fs.readFileSync(filepath, 'utf8');
+
+	  Handlebars.registerPartial(name, template);
+	});
+	return Handlebars.compile(tpl)({
+		css: css,
+		resume: resume
+	});
+}
+
+module.exports = {
+	render: render
+};
