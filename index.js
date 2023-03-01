18253a79b4893ff5d5dd2f6f5f1c7ac12304cb8d
enawder <60314455+enawder@users.noreply.github.com>
2023-03-01 22:14:04 +0000

    Fix indentation

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ var Handlebars = require("handlebars");
 
 function render(resume) {
   var colorscheme = resume.meta.colorscheme;
-	var css = fs.readFileSync(
+  var css = fs.readFileSync(
     __dirname + "/src/pillar-theme/assets/css/" + colorscheme + ".css", "utf-8");
-	var tpl = fs.readFileSync(__dirname + "/src/index.hbs", "utf-8");
-	var partialsDir = path.join(__dirname, '/src/partials');
-	var filenames = fs.readdirSync(partialsDir);
+  var tpl = fs.readFileSync(__dirname + "/src/index.hbs", "utf-8");
+  var partialsDir = path.join(__dirname, '/src/partials');
+  var filenames = fs.readdirSync(partialsDir);
 
-	filenames.forEach(function (filename) {
-	  var matches = /^([^.]+).hbs$/.exec(filename);
-	  if (!matches) {
-	    return;
-	  }
-	  var name = matches[1];
-	  var filepath = path.join(partialsDir, filename)
-	  var template = fs.readFileSync(filepath, 'utf8');
+  filenames.forEach(function (filename) {
+    var matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) {
+      return;
+    }
+    var name = matches[1];
+    var filepath = path.join(partialsDir, filename)
+    var template = fs.readFileSync(filepath, 'utf8');
 
-	  Handlebars.registerPartial(name, template);
-	});
-	return Handlebars.compile(tpl)({
-		css: css,
-		resume: resume
-	});
+    Handlebars.registerPartial(name, template);
+  });
+  return Handlebars.compile(tpl)({
+    css: css,
+    resume: resume
+  });
 }
 
 module.exports = {
-	render: render
+  render: render
 };
