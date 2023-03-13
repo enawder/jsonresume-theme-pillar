1be66cb7228f21f69f0cdd58625836dd36e96d45
enawder <60314455+enawder@users.noreply.github.com>
2023-03-13 20:31:08 +0000

    Export to pdf using resume-cli instead of html2pdf script

--- index.js
+++ index.js
@@ -77,5 +77,18 @@ function render(resume) {
 }
 
 module.exports = {
-  render: render
+  render: render,
+  pdfRenderOptions: {
+    mediaType: "print",
+    format: "A4",
+    landscape: false,
+    margin: {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    },
+    printBackground: true,
+    scale: 0.7
+  }
 };
